246f7e1e92c65bcb7b83e6e3b5d9b679aafa187c
milon27 <mdjahidulislammilon@gmail.com>
2024-12-20 22:28:03 +0600

    shadcn done with manul approch but custom style util not importing properly

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from './view/components/ui/button'
+import Versions from './view/components/versions'
+
 function App(): JSX.Element {
   return (
     <>
       <h1 className="text-3xl font-bold underline text-red-600 text-center">Hello world!</h1>
+      <Button>Click</Button>
+      <Versions />
     </>
   )
 }
